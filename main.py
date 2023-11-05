90177f9fa86f3bcfc63b1ea79953d39b590cccbe
maximghost <136229840+maximghost@users.noreply.github.com>
2023-11-05 12:59:51 -0800

    Add files via upload
    Quelques bouts de codes que j'ai pu écrire en apprenant Python, que des codes simples et classiques pour le début. Je vous recommande " My first game" qui est un jeu très amusant.

---
+++ main.py
@@ -0,0 +1,8 @@
+
+first_name = input (" What's your first name? ")
+last_name = input (" What's your last name? ")
+age = input (" How old are you? ")
+nationality = input (" What's your nationality? ")
+sexe = input (" Male or Female? ")
+
+print(" You are " + first_name + " " + last_name + " " + " and you have " + " " + age + " years old. " + " You are " + nationality + " and is " + sexe + ".")
